0a9d066a2f759b1df3ee0d5bfa10207313376113
PavelSemenyuk <pavel.semenuk.spb@gmail.com>
2023-01-23 09:49:05 +0300

    start commit

---
+++ src/HomeWork.java
@@ -0,0 +1,10 @@
+public class HomeWork {
+    public static void main(String[]args) {
+        task1();
+    }
+
+    public static void task1(){
+        System.out.println();
+        System.out.println("Задача №1");
+    }
+}
